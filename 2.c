da93dc6ec267cc2794180e8d0b7d3cb9ce532ca4
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2022-09-19 12:48:54 -0500

    Update 2.c
    Declaraciones de dos funciones de las 4 a utilizar en pilas

--- 2.c
+++ 2.c
@@ -7,14 +7,44 @@ struct elemento {
 };
 
 //funciones de pila
-int insertar(struct elemento **cima, int nuevo);//doble apuntador por que se modifica la cima
+int insertar(struct elemento **cima, int datoNuevo);//doble apuntador por que se modifica la cima
 int eliminar(struct elemento **cima);
-int insertar2(struct elemento **cima, int nuevo);//doble apuntador por que se modifica la cima
-int eliminar2(struct elemento **cima);
+//int insertar2(struct elemento **cima, int nuevo);//doble apuntador por que se modifica la cima
+//int eliminar2(struct elemento **cima);
 int verPrimerpila(struct elemento *cima); // apuntador normal por que no se modifica la cima
 int vacia(struct elemento *cima);
 
 int main(){
     printf("\n");
     return 0;
+}
+int insertar(struct elemento **cima, int datoNuevo){
+    struct elemento *nuevo;
+    nuevo = (struct elemento*)malloc(sizeof(struct elemento));
+    if(nuevo == NULL){
+        return 0;
+    }
+    nuevo->dato=datoNuevo;
+    nuevo->sig = *cima;
+    *cima = nuevo;
+}
+int eliminar(struct elemento **cima){
+    if(*cima == NULL){
+        return -1;
+    }
+    struct elemento *nodoEliminado;
+    int datoEliminado;
+    nodoEliminado = *cima;//se respalda el nodo para poderlo liberar
+    datoEliminado = nodoEliminado->dato;//se respalda el dato para poderlo devolver 
+    *cima = (*cima)->sig;//flecita es de campo de estructura 
+    free(nodoEliminado);
+    return datoEliminado;
+
+}
+int verPrimerpila(struct elemento *cima){
+
+
+}
+int vacia(struct elemento *cima){
+
 }
